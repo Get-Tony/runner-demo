151e0fe3e8e61b52f8c5504011fc6a47db368197
Get-Tony <Get-Tony@outlook.com>
2023-03-16 11:59:42 +0100

    added proxy option

--- run_service1.sh
+++ run_service1.sh
@@ -8,5 +8,21 @@ if [ ! -d "services/service1" ]; then
     exit 1
 fi
 
-# Run the environment
-ansible-runner run services/service1 -p main.yml --rotate-artifacts 7
+# Get the proxy URL as argument
+if [ -z "$1" ]; then
+    echo "No proxy URL provided. Trying direct access."
+fi
+
+GIT_PROXY_URL=$1
+
+# If the proxy URL is provided, apply it before executing the service
+if [ -n "$GIT_PROXY_URL" ]; then
+    HTTPS_PROXY=$GIT_PROXY_URL ansible-runner run services/service1 -p main.yml --rotate-artifacts 7
+else
+    ansible-runner run services/service1 -p main.yml --rotate-artifacts 7
+fi
+
+# If failed, inform about the proxy URL argument
+if [ $? -ne 0 ]; then
+    echo "ERROR: Execution failed. Try providing a proxy URL as an argument."
+fi
