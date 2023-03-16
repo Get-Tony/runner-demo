0ba1ea0f16d9cae651688fc2b5e9569fe533858c
Get-Tony <Get-Tony@outlook.com>
2023-03-16 02:14:48 +0100

    linted and added envvar activation

--- configure_service1.sh
+++ configure_service1.sh
@@ -7,7 +7,7 @@
   connection: local
   tasks:
     - name: "Check for required commands"
-      command: "{{ item }} --version"
+      ansible.builtin.command: "{{ item }} --version"
       register: required_commands
       failed_when: required_commands.rc != 0
       with_items:
@@ -18,34 +18,34 @@
       changed_when: false
 
     - name: "Fail if required commands are missing"
-      fail:
+      ansible.builtin.fail:
           msg: "Command '{{ item.item }}' not found. Please install it and try again."
       when: item.failed
       with_items: "{{ required_commands.results }}"
 
     - name: "Check if service1 repo exists"
-      stat:
+      ansible.builtin.stat:
           path: services/service1
       register: service1_repo
 
     - name: "Clone the demo service repo"
-      git:
+      ansible.builtin.git:
           repo: https://github.com/get-tony/service_service1.git
           dest: services/service1
       when: not service1_repo.stat.exists
 
     - name: "Create host_vars directory"
-      file:
+      ansible.builtin.file:
           path: services/service1/inventory/host_vars
           state: directory
 
     - name: "Create group_vars directory"
-      file:
+      ansible.builtin.file:
           path: services/service1/inventory/group_vars
           state: directory
 
     - name: "Creating services/service1/inventory/group_vars/controllers.yml"
-      copy:
+      ansible.builtin.copy:
         dest: services/service1/inventory/group_vars/controllers.yml
         content: |
             ---
@@ -55,9 +55,20 @@
                 version: main
 
     - name: "Creating services/service1/inventory/host_vars/localhost.yml"
-      copy:
+      ansible.builtin.copy:
         dest: services/service1/inventory/host_vars/localhost.yml
         content: |
             ---
             ansible_python_interpreter: /usr/bin/python3
             ansible_connection: local
+
+    - name: "Check if services/service1/env/envvars exists"
+      ansible.builtin.stat:
+          path: services/service1/env/envvars
+      register: envvars
+
+    - name: "Activating envvars"
+      ansible.builtin.copy:
+          src: services/service1/env/envvars_demo
+          dest: services/service1/env/envvars
+      when: not envvars.stat.exists
