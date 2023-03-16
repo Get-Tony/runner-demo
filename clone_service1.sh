0fdc4a46185b1a42c793f5d355cb2993c4aebfc9
Get-Tony <Get-Tony@outlook.com>
2023-03-16 01:59:44 +0100

    removed a check that came too early

--- clone_service1.sh
+++ clone_service1.sh
@@ -16,9 +16,3 @@ done
 if [ ! -d "services/service1" ]; then
     git clone https://github.com/get-tony/service_service1.git services/service1
 fi
-
-# Verify the service repo exists
-if [ ! -d "services/service1/inventory/host_vars" ]; then
-    echo "ERROR: service1 is not configured. See its 'services/service1/README.md'"
-    exit 1
-fi
